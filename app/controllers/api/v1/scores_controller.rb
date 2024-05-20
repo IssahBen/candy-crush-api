aca802f4778830dc28dfda73dcfbfe355a092c67
IssahBen <benedictissah@gmail.com>
2024-05-19 19:15:51 -0500

    after save redirect link removed

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::ScoresController < ApplicationController
     @score = Score.new(score_params)
 
     if @score.save
-      render json: @score, status: :created, location: @score
+      render json: @score, status: :created
     else
       render json: @score.errors, status: :unprocessable_entity
     end
